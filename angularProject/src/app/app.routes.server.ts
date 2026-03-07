0e5f07702ef33a655827e6e7193515a620d21cbe
markplaceapi <markplaceapi@gmail.com>
2026-03-08 00:43:03 +0530

    Add products add/edit pages with API integration

--- angularProject/src/app/app.routes.server.ts
+++ angularProject/src/app/app.routes.server.ts
@@ -10,6 +10,10 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Server
   },
   {
+    path: 'configuration/products/edit/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
